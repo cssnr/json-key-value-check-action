c552613c34166e0a3c15e128cd15a0cc6d289615
Shane <6071159+smashedr@users.noreply.github.com>
2025-09-24 21:49:27 -0700

    Update Build, Dependencies and Workflows

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,26 @@
-import { fixupPluginRules } from '@eslint/compat'
-import { FlatCompat } from '@eslint/eslintrc'
 import js from '@eslint/js'
 import typescriptEslint from '@typescript-eslint/eslint-plugin'
-// import tsParser from '@typescript-eslint/parser'
-import _import from 'eslint-plugin-import'
-// import prettier from 'eslint-plugin-prettier'
+import tsParser from '@typescript-eslint/parser'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
 
-const compat = new FlatCompat({
-    baseDirectory: '.',
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all,
+export default defineConfig(js.configs.recommended, tseslint.configs.recommended, {
+    plugins: {
+        '@typescript-eslint': typescriptEslint,
+    },
+    languageOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        parser: tsParser,
+    },
+    settings: {
+        env: {
+            node: true,
+            es2021: true,
+        },
+    },
+    rules: {
+        'no-undef': 'off',
+        'no-extra-semi': 'off',
+    },
 })
-
-export default [
-    {
-        ignores: ['**/dist', '**/node_modules'],
-    },
-    ...compat.extends(
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended'
-        // 'plugin:prettier/recommended'
-    ),
-    {
-        plugins: {
-            import: fixupPluginRules(_import),
-            // prettier,
-            '@typescript-eslint': typescriptEslint,
-        },
-        rules: {
-            '@typescript-eslint/no-explicit-any': 'off',
-        },
-    },
-]
